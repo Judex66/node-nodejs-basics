9e0118571fb5ddf4b7c2c1449bd958d8c3fdfc23
Judex66 <sermac2000@mail.ru>
2024-10-02 17:22:08 +0300

    cp

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,16 +1,14 @@
-import { copyFile, readdir, mkdir } from 'fs/promises';
+import { cp } from 'fs/promises';
+
 const copy = async () => {
   const fileWay ='src/fs/files'
     const newFileCopy='new_file_copy'
     try{
-    const [files]= await Promise.all([readdir(fileWay), mkdir(`${fileWay}/${newFileCopy}`)])
-    const copyFiles = files.map((newFiles)=>
-      copyFile(`${fileWay}/${newFiles}`, `${fileWay}/${newFileCopy}/${newFiles}`)
-    )
-    await Promise.all(copyFiles)}
+    await cp(fileWay, newFileCopy, { recursive: true });
+  }
     catch(error){
       console.log('FS operation failed')
-      //console.log(error)
+      console.log(error)
     }
 };
 
