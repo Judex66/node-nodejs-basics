a4802e3b646302882a11e1c2b599f6ff83035d51
Judex66 <sermac2000@mail.ru>
2024-10-06 10:44:40 +0300

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,13 +1,6 @@
-import { fork } from 'child_process';
-import { dirname, join } from "node:path";
-import { fileURLToPath } from "node:url";
 const spawnChildProcess = async (args) => {
-  const dirname_new = dirname(fileURLToPath(import.meta.url));
-let child = fork(join(dirname_new, "files", "script.js"), args);
-
-child.on('message', function (args) {
-    console.log(args);
-});
+  // Write your code here
 };
 
-spawnChildProcess(['test1', 'test2', 'test3']);
+// Put your arguments in function call to test this functionality
+spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
