992143f5532a14fd6f275c4deabc922b61875882
Judex66 <sermac2000@mail.ru>
2024-10-06 10:46:31 +0300

    transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -2,7 +2,7 @@ import { Transform } from 'stream';
 import { pipeline } from 'stream/promises';
 const transform = async () => {
     const transformationProcces = new Transform({
-        transform(chunk, nul, transformation) {
+        transform(chunk, _, transformation) {
             transformation(null, chunk.toString().split('').reverse().join(''));
         }
     })
